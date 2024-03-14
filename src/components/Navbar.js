68f5e8c365e914de70ad7ee66cc5e581dd69854c
Dhruv-Swami <dhruvswaminhr@gmail.com>
2024-03-14 12:54:44 +0530

    initiate

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -36,37 +36,9 @@ export default function Navbar(props) {
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Pricing
-                </a>
-              </li>
-              <li class="nav-item dropdown">
-                <a
-                  class="nav-link dropdown-toggle"
-                  href="#"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown link
-                </a>
-                <ul class="dropdown-menu">
-                  <li>
-                    <a class="dropdown-item" href="#">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="#">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" herf="/">
-                      {props.abouttext}
-                    </a>
-                  </li>
-                </ul>
+                <Link class="nav-link" to="/about">
+                  About
+                </Link>
               </li>
             </ul>
           </div>
